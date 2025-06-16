a4fcf029970448a76baba457665cb512237e58b3
Arizoux <felixhasel3@gmx.de>
2025-06-16 16:41:28 +0200

    added comma to cors middleware

--- restaurant_ms/settings.py
+++ restaurant_ms/settings.py
@@ -50,7 +50,7 @@ REST_FRAMEWORK = {
 """
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
